03d99976e9c0872f55f6cf99d847a00f161a1987
Jan <jolschew@web.de>
2016-04-25 14:23:36 +0200

    created new Classes for DB-Creation and CRUD-Operations

---
+++ ToDo/app/src/main/java/todo/kebejaol/todo/DBHelper.java
@@ -0,0 +1,9 @@
+package todo.kebejaol.todo;
+
+/**
+ * Created by Jan on 25.04.16.
+ *
+ * Create new Database if no DB exists
+ */
+public class DBHelper {
+}
---
+++ ToDo/app/src/main/java/todo/kebejaol/todo/LoginDBAdapter.java
@@ -0,0 +1,9 @@
+package todo.kebejaol.todo;
+
+/**
+ * Created by Jan on 25.04.16.
+ *
+ * Handle CRUD Operations
+ */
+public class LoginDBAdapter {
+}
