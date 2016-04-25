7a3ff3e00d63a4e9a50bd0ac73e136c621aa278a
Jan <jolschew@web.de>
2016-04-25 15:13:40 +0200

    implemented CRUD Methods for LoginDBAdapter.class

--- ToDo/app/src/main/java/todo/kebejaol/todo/LoginDBAdapter.java
+++ ToDo/app/src/main/java/todo/kebejaol/todo/LoginDBAdapter.java
@@ -1,10 +1,100 @@
 package todo.kebejaol.todo;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
 /**
  * Created by Jan on 25.04.16.
  *
  * Handle CRUD Operations
  */
 public class LoginDBAdapter {
-    public static final String DB_CREATE = "";
+    public static final String DB_NAME = "login.db";
+    static final int DB_VERSION = 1;
+    static final int NAME_COLUMN = 1;
+
+    //Database Creation Statement
+    public static final String DB_CREATE = "create table "+ "LOGIN"+ "( " +"ID"+" integer primary key autoincrement,"+ "EMAIL  text,PASSWORD integer); ";
+
+    //instance of database
+    public SQLiteDatabase db;
+    //Context of application using database
+    private final Context context;
+
+    private DBHelper dbHelper;
+
+    public LoginDBAdapter(Context context) {
+        this.context = context;
+        dbHelper = new DBHelper(this.context, DB_NAME, null, DB_VERSION);
+    }
+
+    public LoginDBAdapter open() throws SQLException
+    {
+        db = dbHelper.getWritableDatabase();
+        return this;
+    }
+    public void close()
+    {
+        db.close();
+    }
+
+    public SQLiteDatabase getDBInstance()
+    {
+        return db;
+    }
+
+    // CREATE
+    public void insertEntry(String eMail, int password)
+    {
+        ContentValues registerValues = new ContentValues();
+        // Assign Values to DB
+        registerValues.put("EMAIL", eMail);
+        registerValues.put("PASSWORD", password);
+
+        //Insert row in table
+        db.insert("LOGIN", null, registerValues);
+    }
+
+    // READ
+    // TODO Encrypt Password with Hash-Code
+    public String getEntry (String eMail)
+    {
+        Cursor cursor = db.query("LOGIN", null, "EMAIL=?", new String[]{eMail}, null, null, null);
+
+        //If User does not exist
+        if (cursor.getCount() < 1)
+        {
+            cursor.close();
+            return "Dieser Nutzer existiert nicht!";
+        }
+        cursor.moveToFirst();
+        String password = cursor.getString(cursor.getColumnIndex("PASSWORD"));
+        cursor.close();
+        return password;
+    }
+
+    // UPDATE
+    public void updateEntry(String eMail, int password)
+    {
+        ContentValues updatedValues = new ContentValues();
+        updatedValues.put("EMAIL", eMail);
+        updatedValues.put("PASSWORD", password);
+
+        String user = "EMAIL=?";
+        db.update("LOGIN", updatedValues, user, new String[]{eMail});   
+
+    }
+
+    // DELETE
+    public int deleteEntry(String eMail)
+    {
+        String user = "EMAIL=?";
+        int numberofEntriesDeleted = db.delete("LOGIN", user, new String[]{eMail});
+        return numberofEntriesDeleted;
+    }
+
+
 }
